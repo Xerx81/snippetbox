1b0d73e2e8598df3f9df0c178e8c7c3df0cfe048
Xerx81 <ryuvraj832@gmail.com>
2025-06-19 12:13:24 +0530

    setup go web app

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Xerx81/snippetbox
+
+go 1.24.3
---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from Snippetbox"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	msg := fmt.Sprintf("Display a specific snippet with ID %d...", id)
+	w.Write([]byte(msg))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a form for creating snippet."))
+}
+
+func main() {
+	// Initialize a new servemux
+	mux := http.NewServeMux()
+
+	// Routes
+	mux.HandleFunc("/{$}", home)
+	mux.HandleFunc("/snippet/view/{id}", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
+
+	// Print a log message to say that the server is starting
+	log.Print("server starting on http://localhost:4000")
+
+	// Start a new web server
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+}
